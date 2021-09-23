e2ed3bfc92ce401ae9d9f6dcf15e9003a2d24227
dimantarian <guylbourne@icloud.com>
2021-09-23 21:47:04 +1000

    updated test cases

--- test_main.py
+++ test_main.py
@@ -4,6 +4,7 @@ import pprint
 import tkinter as tk
 import logging
 from connectors.swyftx import SwyftxClient
+from connectors.btcmarkets import BTCMarketsClient
 from creds.creds import creds
 
 # Setup Logging
@@ -24,17 +25,32 @@ logger.addHandler(stream_handler)
 logger.addHandler(file_handler)
 
 if __name__ == '__main__':
-    # Get data
-    swyftx = SwyftxClient(False, creds['demoPublicKey'])
+    # # Get data
+    swyftx = SwyftxClient(False, creds['prodPublicKey'])
+    #
+    # # Test private endpoint
     pprint.pprint(swyftx.get_balance())
-    # pprint.pprint(swyftx.place_order("USD", "SOL", "5000", "USD", 1, None))
-    pprint.pprint(swyftx.get_historical_candles("BTC", "1h", None, None))
-    pprint.pprint(swyftx.get_bid_ask("BTC"))
-
     # pprint.pprint(swyftx.get_assets())
+    #
+    # # swyftx.place_order("USD", "SOL", "100", "USD", 3, 200)
+    #
+    # # Test pulling back the first/last order
+    # oldest_order = swyftx.get_all_orders("SOL")['orders'][0]['orderUuid']
+    # pprint.pprint(swyftx.get_order_status(oldest_order))
+    # most_recent_order = swyftx.get_all_orders("SOL")['orders'][-1]['orderUuid']
+    # pprint.pprint(swyftx.get_order_status(most_recent_order))
+    #
+    # # Test cancelling an order
+    # swyftx.place_order("USD", "SOL", "2000", "USD", 3, 25)
+    # most_recent_order = swyftx.get_all_orders("SOL")['orders'][-1]['orderUuid']
+    # swyftx.cancel_order(most_recent_order)
+
+    # Test BTCMarkets Websocket
+    # btc = BTCMarketsClient(creds['btcPublicKey'],
+    #                        creds['btcPrivateKey'])
 
     # Instantiate a TK object (application)
-    root = tk.Tk()
+    # root = tk.Tk()
 
     # A loop is required to prevent the program from terminating
-    root.mainloop()
+    # root.mainloop()
